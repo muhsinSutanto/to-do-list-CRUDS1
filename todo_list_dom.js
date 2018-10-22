78f54420c2c5c8bbd7eb6ff4ff1c016210b7c828
Muhammad Haekal <93haekal@gmail.com>
2018-10-22 14:22:30 +0700

    Finish Todo List

--- todo_list_dom.js
+++ todo_list_dom.js
@@ -1,63 +1,93 @@
 const container = document.getElementById("container")// container todo list
 const inputTodo = document.getElementById("inputTodo")// input dari user
 const submitButton = document.getElementById("submitButton")// tombol submit
+const inputUpdate = document.getElementById("inputUpdate")// update value
+const updateButton = document.getElementById("updateButton")// update value
+const inputSearch = document.getElementById("inputSearch")// input search
 
 // Event Listener ---------------------------------------
-submitButton.addEventListener("click",add) 
+submitButton.addEventListener("click", add)
+inputSearch.addEventListener("keyup", search)
 
 // Array of object data todo list
 let todos = []
 
 function add() {
     let description = inputTodo.value
-
+    inputTodo.value = ""
     todos.push({
         description: description,
         done: false
     })
 
-    read()
+    display()
 }
 
-function read() {
+function display() {
     let todoList = ""
-    todos.forEach(function (todo) {
-        todoList = todoList + `<div>${todo.description}</div>`
+    todos.forEach(function (todo, index) {
+        todoList = todoList + `<div onDblclick="prepareUpdate(${index})" class="todo">
+                                    <span>${todo.description}</span>   
+                                    <span onClick="remove(${index})">X</span>
+                                </div>`
     })
     container.innerHTML = todoList
 }
 
-function update(index, key, value) {
-    todos[index][key] = value
+function prepareUpdate(index) {
+    inputUpdate.style.display = "inline-block"
+    updateButton.style.display = "inline-block"
+    inputUpdate.value = todos[index].description
+    updateButton.onclick = function () {
+        update(index)
+    }
+}
+
+function update(index) {
+    todos[index]["description"] = inputUpdate.value
+    display()
 }
 
 function remove(index) {
     todos.splice(index, 1)
+    display()
 }
 
-function search1(key, value) {
+function search() {
     let result = todos.filter(
         function (todo) {
-            return todo[key] === value
+            return todo["description"].toLowerCase().includes(inputSearch.value.toLowerCase())
         }
     )
-    console.log(result)
+    if (inputSearch.value === "") {
+        display()
+    } else {
+        let todoList = ""
+        result.forEach(function (todo, index) {
+            todoList = todoList + `<div onDblclick="prepareUpdate(${index})" class="todo">
+                                        <span>${todo.description}</span>   
+                                        <span onClick="remove(${index})">X</span>
+                                    </div>`
+        })
+        container.innerHTML = todoList
+    }
+
 }
 
-function search2(key, value) {
-    let result = todos.filter(
-        function (todo) {
-            if (typeof todo[key] === "string") {
-                return todo[key].toLowerCase().includes(value.toLowerCase())
-            } else {
-                return todo[key] === value
-            }
-        }
-    )
-    console.log(result)
-}
+// function search2(key, value) {
+//     let result = todos.filter(
+//         function (todo) {
+//             if (typeof todo[key] === "string") {
+//                 return todo[key].toLowerCase().includes(value.toLowerCase())
+//             } else {
+//                 return todo[key] === value
+//             }
+//         }
+//     )
+//     console.log(result)
+// }
 
-read()
+display()
 
 
 
